f64b5c0655892b398aeac44416cee8d242fde669
duyk30b <duyk30b@gmail.com>
2021-12-18 23:32:45 +0700

    try menu bar

--- tree-menu/functions.js
+++ tree-menu/functions.js
@@ -6,27 +6,38 @@ class TreeMenuElement {
 	listenEvent() {
 		const that = this
 		this.treeMenu.addEventListener('click', e => {
-			if (e.target.closest('tree-item')) {
-				const target = e.target.closest('tree-item')
+			if (e.target.closest('.tree-item')) {
+				const target = e.target.closest('.tree-item')
 				const data = target.dataset.treeItem
 				that.activeItem(data)
 			}
-			if (e.target.closest('tree-group-title')) {
-				const treeGroupTitle = e.target.closest('tree-group-title')
+			if (e.target.closest('.tree-group-title')) {
+				const treeGroupTitle = e.target.closest('.tree-group-title')
 				const treeGroup = treeGroupTitle.parentElement
 				that.activeGroup(treeGroup)
 			}
 		})
 	}
+	setMaxHeightTreeGroupChildren(treeGroup, bool) {
+		// const treeGroupChildren = treeGroup.querySelector('.tree-group-children')
+		// if (bool) {
+		// 	// treeGroupChildren.style.maxHeight = treeGroupChildren.scrollHeight + 'px'
+		// 	treeGroupChildren.style.maxHeight = '1000px'
+		// } else {
+		// 	treeGroupChildren.style.maxHeight = null
+		// }
+	}
 	clearAllActiveItem() {
-		this.treeMenu.querySelectorAll('tree-item').forEach(element => {
+		this.treeMenu.querySelectorAll('.tree-item').forEach(element => {
 			element.classList.remove('active')
 		})
 	}
 	clearAllExpandGroup() {
-		this.treeMenu.querySelectorAll('tree-group').forEach(item => {
+        const that = this
+		this.treeMenu.querySelectorAll('.tree-group').forEach(item => {
 			if (item.classList.contains('expand')) {
 				item.classList.remove('expand')
+				that.setMaxHeightTreeGroupChildren(item, false)
 			}
 		})
 	}
@@ -35,36 +46,41 @@ class TreeMenuElement {
 		this.clearAllExpandGroup()
 	}
 	expandGroupAndParentByElement(element) {
+        const that = this
 		let treeGroup = element
 		while (treeGroup) {
-			if (treeGroup.nodeName === 'TREE-GROUP') {
+			if (treeGroup.classList.contains('tree-group')) {
 				treeGroup.classList.add('expand')
+				that.setMaxHeightTreeGroupChildren(treeGroup, true)
 			}
 			treeGroup = treeGroup.parentElement
 		}
 	}
 	closeExpandGroupAndChildrenByElement(element) {
-		element.querySelectorAll('tree-group').forEach(item => {
+        const that = this
+		element.querySelectorAll('.tree-group').forEach(item => {
 			item.classList.remove('expand')
+			that.setMaxHeightTreeGroupChildren(item, false)
 		})
 	}
 	activeItem(data) {
 		this.clearAllActiveExpand()
-		const treeItem = this.treeMenu.querySelector(`tree-item[data-tree-item='${data}']`)
+		const treeItem = this.treeMenu.querySelector(`.tree-item[data-tree-item='${data}']`)
 		this.expandGroupAndParentByElement(treeItem)
 		treeItem.classList.add('active')
 	}
 	activeGroup(treeGroup) {
 		if (treeGroup.classList.contains('expand')) {
-			this.closeExpandGroupAndChildrenByElement(treeGroup)
-            treeGroup.classList.remove('expand')
+			treeGroup.classList.remove('expand')
+			// this.closeExpandGroupAndChildrenByElement(treeGroup)
 		} else {
-			this.clearAllExpandGroup()
-			this.expandGroupAndParentByElement(treeGroup)
+            treeGroup.classList.add('expand')
+			// this.clearAllExpandGroup()
+			// this.expandGroupAndParentByElement(treeGroup)
 		}
 	}
 }
 
 window.addEventListener('load', () => {
-	new TreeMenuElement(document.getElementById('tree-menu'))
+	new TreeMenuElement(document.getElementById('tree-example'))
 })
